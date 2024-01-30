68a69f76a72418fd0c7a4cad1eb922afa0bcf2bd
gianfrancoscapin <gianscapin96@gmail.com>
2024-01-30 20:35:22 -0300

    add GroceryCategories DTOs

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gianfranco Scapin on 30/01/2024.
+//
+
+import Foundation
+
+public struct GroceryCategoryDTO: Codable {
+    
+    public let title: String
+    public let color: String
+    
+    public init(title: String, color: String) {
+        self.title = title
+        self.color = color
+    }
+    
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gianfranco Scapin on 30/01/2024.
+//
+
+import Foundation
